fb8e306464160a7f0c26a485ae7e8344508446ec
ggn <ggn.dbug@gmail.com>
2015-06-04 10:37:11 +0300

    Added FPU tester

---
+++ ASM/Various/FPU card tester/DRI/FPTEST.C
@@ -0,0 +1,7 @@
+#include <math.h>                               /* Einbindung der Mathe Bibliothek     */
+
+main()
+{
+   mctest();                            /* MC68881 testen & vorbereiten  */
+   printf("MC 68881 ist vorhanden\n");  /* Print falls MC68881 vorhanden */
+}
